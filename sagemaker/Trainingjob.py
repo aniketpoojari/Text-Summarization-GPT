104ecaf135dafd27738ce3785dc961e66d2d3dd9
ANIKET POOJARI <aniketp2009@gmail.com>
2024-11-28 23:34:06 -0800

    sagemaker training

--- sagemaker/Trainingjob.py
+++ sagemaker/Trainingjob.py
@@ -2,10 +2,10 @@ from sagemaker.inputs import TrainingInput
 from sagemaker import get_execution_role
 from sagemaker.estimator import Estimator
 
-role = "arn:aws:iam::975050169307:role/training-jobs-role"
+role = "arn:aws:iam::<id>:role/<role>"
 
 estimator = Estimator(
-    image_uri="975050169307.dkr.ecr.us-west-2.amazonaws.com/gpt",
+    image_uri="<id>.dkr.ecr.<region>.amazonaws.com/<repo>",
     role=role,
     instance_type="ml.g4dn.xlarge",  # Choose your instance type
     instance_count=1,
@@ -15,7 +15,7 @@ estimator = Estimator(
     max_run=3600,   # Maximum run time (in seconds)
     input_mode="File",
     output_dir="/opt/ml/model/",
-    output_path="s3://text-summarization-gpt-aniket/",
+    output_path="s3://<bucket>/",
     environment={  # Override environment variables here
         "vocab_size": "50257",
         "n_embd": "768",
@@ -35,11 +35,11 @@ estimator = Estimator(
 
 # Define the input data location (could be S3 or other)
 s3_input_train = TrainingInput(
-    s3_data="s3://text-summarization-gpt-aniket/pretraining_train.pkl"
+    s3_data="s3://<bucket>/pretraining_train.pkl"
 )
 
 s3_input_validation = TrainingInput(
-    s3_data="s3://text-summarization-gpt-aniket/pretraining_val.pkl"
+    s3_data="s3://<bucket>/pretraining_val.pkl"
 )
 
 # Start the training job
